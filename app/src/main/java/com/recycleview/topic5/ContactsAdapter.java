c76746ebed3f21794b725b29ff5b500a1ea0219b
Ajar-maharjan <pleiades.ajar@gmail.com>
2019-12-06 14:36:52 +0545

    Recyclerview adapter

---
+++ app/src/main/java/com/recycleview/topic5/ContactsAdapter.java
@@ -0,0 +1,45 @@
+package com.recycleview.topic5;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactViewHolder>{
+
+    Context context;
+    List<Contacts> contactsList;
+    @NonNull
+    @Override
+    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgProfile;
+        TextView tvName, tvPhone;
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgProfile = itemView.findViewById(R.id.imgProfile);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvPhone = itemView.findViewById(R.id.tvPhone);
+        }
+    }
+}
